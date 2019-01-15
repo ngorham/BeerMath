062d060e4fca273e531c736541ecd6cab2312958
ngorham <ngorham2@gmail.com>
2019-01-14 22:31:25 -0800

    LiquidPackagingCalculator Update 1 1/14/2019
    Added ounce, pint and gallon conversion functions to LiquidCalculator

--- app/src/main/java/net/ngorham/liquidpackagingcalculator/LiquidCalculator.java
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/LiquidCalculator.java
@@ -12,8 +12,13 @@ package net.ngorham.liquidpackagingcalculator;
  */
 
 public class LiquidCalculator {
-    //Constants
+    //Private Constants
     private final double PINT_IN_OZ = 16.0;
+    private final double GALLON_IN_OZ = 128.0;
+    private final double BARREL_IN_OZ = 4032.0;
+    private final double MILLILITER_IN_OZ = 0.033814;
+    private final double LITER_IN_OZ = 33.814;
+    private final double BARREL_IN_GALLON = 31.5;
 
     //Default Constructor
     public LiquidCalculator(){}
@@ -23,13 +28,79 @@ public class LiquidCalculator {
     public double ounceToOunce(double val){
         return val;
     }
-
     //Convert ounce to pint
     public double ounceToPint(double val){
         return (val / PINT_IN_OZ);
     }
+    //Convert ounce to gallon
+    public double ounceToGallon(double val){
+        return (val / GALLON_IN_OZ);
+    }
+    //Convert ounce to barrel
+    public double ounceToBarrel(double val){
+        return (val / BARREL_IN_OZ);
+    }
+    //Convert ounce to milliliter
+    public double ounceToMilliliter(double val){
+        return (val / MILLILITER_IN_OZ);
+    }
+    //Convert ounce to liter
+    public double ounceToLiter(double val){
+        return (val / LITER_IN_OZ);
+    }
+
     //Pint conversion functions
+    //Convert pint to ounce
+    public double pintToOunce(double val){
+        return (val * PINT_IN_OZ);
+    }
+    //Convert pint to pint
+    public double pintToPint(double val){
+        return val;
+    }
+    //Convert pint to gallon
+    public double pintToGallon(double val){
+        return (ounceToGallon(pintToOunce(val)));
+    }
+    //Convert pint to barrel
+    public double pintToBarrel(double val){
+        return (ounceToBarrel(pintToOunce(val)));
+    }
+    //Convert pint to milliliter
+    public double pintToMilliliter(double val){
+        return (ounceToMilliliter(pintToOunce(val)));
+    }
+    //Convert pint to liter
+    public double pintToLiter(double val){
+        return (ounceToLiter(pintToOunce(val)));
+    }
+
     //Gallon conversion functions
+    //Convert gallon to ounce
+    public double gallonToOunce(double val){
+        return (val * GALLON_IN_OZ);
+    }
+    //Convert gallon to pint
+    public double gallonToPint(double val){
+        return (ounceToPint(gallonToOunce(val)));
+    }
+    //Convert gallon to gallon
+    public double gallonToGallon(double val){
+        return val;
+    }
+    //Convert gallon to barrel
+    public double gallonToBarrel(double val){
+        return (val / BARREL_IN_GALLON);
+    }
+    //Convert gallon to milliliter
+    public double gallonToMilliliter(double val){
+        return (ounceToMilliliter(gallonToOunce(val)));
+    }
+    //Convert gallon to liter
+    public double gallonToLiter(double val){
+        return (ounceToLiter(gallonToOunce(val)));
+    }
+
     //Barrel conversion functions
     //Milliliter conversion functions
     //Liter conversion functions
--- app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
@@ -18,9 +18,45 @@ public class MainActivity extends AppCompatActivity {
         //Set up Liquid Calculator
         lq = new LiquidCalculator();
         //Set up textViews
+        //Ounces
+        /*
         TextView ounceToOunce = (TextView) findViewById(R.id.ounce_to_ounce_text);
         TextView ounceToPint = (TextView) findViewById(R.id.ounce_to_pint_text);
+        TextView ounceToGallon = (TextView) findViewById(R.id.ounce_to_gallon_text);
+        TextView ounceToBarrel = (TextView) findViewById(R.id.ounce_to_barrel_text);
+        TextView ounceToMilliliter = (TextView) findViewById(R.id.ounce_to_milliliter_text);
+        TextView ounceToLiter = (TextView) findViewById(R.id.ounce_to_liter_text);
         ounceToOunce.setText(String.valueOf(lq.ounceToOunce(1)));
-        ounceToPint.setText(String.valueOf(lq.ounceToPint(36)));
+        ounceToPint.setText(String.valueOf(lq.ounceToPint(1)));
+        ounceToGallon.setText(String.valueOf(lq.ounceToGallon(1)));
+        ounceToBarrel.setText(String.valueOf(lq.ounceToBarrel(1)));
+        ounceToMilliliter.setText(String.valueOf(lq.ounceToMilliliter(1)));
+        ounceToLiter.setText(String.valueOf(lq.ounceToLiter(1)));
+        //Pints
+        TextView pintToOunce = (TextView) findViewById(R.id.pint_to_ounce_text);
+        TextView pintToPint = (TextView) findViewById(R.id.pint_to_pint_text);
+        TextView pintToGallon = (TextView) findViewById(R.id.pint_to_gallon_text);
+        TextView pintToBarrel = (TextView) findViewById(R.id.pint_to_barrel_text);
+        TextView pintToMilliliter = (TextView) findViewById(R.id.pint_to_milliliter_text);
+        TextView pintToLiter = (TextView) findViewById(R.id.pint_to_liter_text);
+        pintToOunce.setText(String.valueOf(lq.pintToOunce(1)));
+        pintToPint.setText(String.valueOf(lq.pintToPint(1)));
+        pintToGallon.setText(String.valueOf(lq.pintToGallon(1)));
+        pintToBarrel.setText(String.valueOf(lq.pintToBarrel(1)));
+        pintToMilliliter.setText(String.valueOf(lq.pintToMilliliter(1)));
+        pintToLiter.setText(String.valueOf(lq.pintToLiter(1)));*/
+        //Gallons
+        TextView gallonToOunce = (TextView) findViewById(R.id.gallon_to_ounce_text);
+        TextView gallonToPint = (TextView) findViewById(R.id.gallon_to_pint_text);
+        TextView gallonToGallon = (TextView) findViewById(R.id.gallon_to_gallon_text);
+        TextView gallonToBarrel = (TextView) findViewById(R.id.gallon_to_barrel_text);
+        TextView gallonToMilliliter = (TextView) findViewById(R.id.gallon_to_milliliter_text);
+        TextView gallonToLiter = (TextView) findViewById(R.id.gallon_to_liter_text);
+        gallonToOunce.setText(String.valueOf(lq.gallonToOunce(1)));
+        gallonToPint.setText(String.valueOf(lq.gallonToPint(1)));
+        gallonToGallon.setText(String.valueOf(lq.gallonToGallon(1)));
+        gallonToBarrel.setText(String.valueOf(lq.gallonToBarrel(1)));
+        gallonToMilliliter.setText(String.valueOf(lq.gallonToMilliliter(1)));
+        gallonToLiter.setText(String.valueOf(lq.gallonToLiter(1)));
     }
 }
