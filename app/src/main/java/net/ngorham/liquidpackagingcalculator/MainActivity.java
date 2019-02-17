e8b4ca25c9ba4bdff96f3825547894f50ab5623b
ngorham <ngorham2@gmail.com>
2019-02-17 15:41:47 -0800

    LiquidPackagingCalculator Update 3 2/17/2019
    Updated Gradle
    Added Toolbar
    Added resources for About, Share and Rate toolbar menu options
    Added About, Share and Rate toolbar menu options

--- app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
@@ -1,20 +1,33 @@
 package net.ngorham.liquidpackagingcalculator;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
     //Private constants
     private static final String TAG = "MainActivity";
     //Private variables
+    private Context context;
     private LiquidCalculator lq;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "INSIDE onCreate: called");
+        context = this;
+        //Set up Action bar
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         //Set up Liquid Calculator
         lq = new LiquidCalculator();
         //Set up textViews
@@ -58,5 +71,103 @@ public class MainActivity extends AppCompatActivity {
         gallonToBarrel.setText(String.valueOf(lq.gallonToBarrel(1)));
         gallonToMilliliter.setText(String.valueOf(lq.gallonToMilliliter(1)));
         gallonToLiter.setText(String.valueOf(lq.gallonToLiter(1)));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.d(TAG, "INSIDE onPostCreate: called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "INSIDE onStart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "INSIDE onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "INSIDE onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "INSIDE onStop: called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "INSIDE onDestroy: called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "INSIDE onRestart: called");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "INSIDE onBackPressed: called");
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //Display icons and text in overflow menu
+        //code found on stackoverflow
+        //https://stackoverflow.com/questions/18374183/how-to-show-icons-in-overflow-menu-in-actionbar
+        if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+            try {
+                Method m = menu.getClass().getDeclaredMethod(
+                        "setOptionalIconsVisible", Boolean.TYPE);
+                m.setAccessible(true);
+                m.invoke(menu, true);
+            } catch (NoSuchMethodException e) {
+                Log.e("onCreateOptionsMenu", "Did not attach icons", e);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Call when user clicks an item in action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) { //Handle action items
+            case R.id.rate_us: //Rate us action
+                Utilities.rateUs(context);
+                return true;
+            case R.id.app_share: //Share action
+                Utilities.shareApp(context);
+                return true;
+            case R.id.app_about: //About action
+                Utilities.aboutDialog(context);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Called when invalidateOptionsMenu() is called
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
---
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/Utilities.java
@@ -0,0 +1,48 @@
+package net.ngorham.liquidpackagingcalculator;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class Utilities {
+    //Display About AlertDialog
+    public static void aboutDialog(final Context context){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.app_about);
+        builder.setIcon(R.drawable.ic_information_black_18dp);
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.dialog_about, null);
+        builder.setView(view);
+        TextView signature = view.findViewById(R.id.signature_text);
+        signature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String website = context.getResources().getString(R.string.website);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+                context.startActivity(webIntent);
+            }
+        });
+        builder.setCancelable(true);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    //Share intent
+    public static void shareApp(Context context){
+        String download = context.getResources().getString(R.string.download_link);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, download);
+        context.startActivity(intent);
+    }
+    //Rate us
+    public static void rateUs(Context context){
+        String site = context.getResources().getString(R.string.play_store);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(site));
+        context.startActivity(intent);
+    }
+}
