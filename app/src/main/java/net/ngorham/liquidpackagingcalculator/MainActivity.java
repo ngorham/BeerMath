0ee5eef3234132c134892af3aba898b6ac2e5914
ngorham <ngorham2@gmail.com>
2019-02-26 23:36:59 -0800

    LiquidPackagingCalculator Update 4 2/26/2019
    Created Volume fragment layout
    Removed test code from MainActivity

--- app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/MainActivity.java
@@ -27,8 +27,7 @@ import java.lang.reflect.Method;
  *
  */
 
-public class MainActivity extends AppCompatActivity
-    implements VolumeFragment.OnVolumeFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
     //Private constants
     private static final String TAG = "MainActivity";
     //Private variables
@@ -38,10 +37,6 @@ public class MainActivity extends AppCompatActivity
     private FragmentManager fm;
     private FragmentTransaction ft;
     private LiquidCalculator lq;
-
-    //OnVolumeFragmentInteractionListener
-    @Override
-    public void onVolumeFragmentInteraction(Uri uri){}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +49,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(myToolbar);
         //Set up Fragment layout
         setUpFragmentLayout();
-        /*Fragment fragment = new VolumeFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.content_frame, fragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();*/
         //Set up Liquid Calculator
         lq = new LiquidCalculator();
         //Set up textViews
@@ -76,33 +65,7 @@ public class MainActivity extends AppCompatActivity
         ounceToGallon.setText(String.valueOf(lq.ounceToGallon(1)));
         ounceToBarrel.setText(String.valueOf(lq.ounceToBarrel(1)));
         ounceToMilliliter.setText(String.valueOf(lq.ounceToMilliliter(1)));
-        ounceToLiter.setText(String.valueOf(lq.ounceToLiter(1)));
-        //Pints
-        TextView pintToOunce = (TextView) findViewById(R.id.pint_to_ounce_text);
-        TextView pintToPint = (TextView) findViewById(R.id.pint_to_pint_text);
-        TextView pintToGallon = (TextView) findViewById(R.id.pint_to_gallon_text);
-        TextView pintToBarrel = (TextView) findViewById(R.id.pint_to_barrel_text);
-        TextView pintToMilliliter = (TextView) findViewById(R.id.pint_to_milliliter_text);
-        TextView pintToLiter = (TextView) findViewById(R.id.pint_to_liter_text);
-        pintToOunce.setText(String.valueOf(lq.pintToOunce(1)));
-        pintToPint.setText(String.valueOf(lq.pintToPint(1)));
-        pintToGallon.setText(String.valueOf(lq.pintToGallon(1)));
-        pintToBarrel.setText(String.valueOf(lq.pintToBarrel(1)));
-        pintToMilliliter.setText(String.valueOf(lq.pintToMilliliter(1)));
-        pintToLiter.setText(String.valueOf(lq.pintToLiter(1)));*/
-        //Gallons
-        TextView gallonToOunce = (TextView) findViewById(R.id.gallon_to_ounce_text);
-        TextView gallonToPint = (TextView) findViewById(R.id.gallon_to_pint_text);
-        TextView gallonToGallon = (TextView) findViewById(R.id.gallon_to_gallon_text);
-        TextView gallonToBarrel = (TextView) findViewById(R.id.gallon_to_barrel_text);
-        TextView gallonToMilliliter = (TextView) findViewById(R.id.gallon_to_milliliter_text);
-        TextView gallonToLiter = (TextView) findViewById(R.id.gallon_to_liter_text);
-        gallonToOunce.setText(String.valueOf(lq.gallonToOunce(1)));
-        gallonToPint.setText(String.valueOf(lq.gallonToPint(1)));
-        gallonToGallon.setText(String.valueOf(lq.gallonToGallon(1)));
-        gallonToBarrel.setText(String.valueOf(lq.gallonToBarrel(1)));
-        gallonToMilliliter.setText(String.valueOf(lq.gallonToMilliliter(1)));
-        gallonToLiter.setText(String.valueOf(lq.gallonToLiter(1)));
+        ounceToLiter.setText(String.valueOf(lq.ounceToLiter(1)));*/
     }
 
     @Override
@@ -205,7 +168,7 @@ public class MainActivity extends AppCompatActivity
 
     //Set up Fragment layout
     private void setUpFragmentLayout(){
-        volumeFrag = VolumeFragment.newInstance("volume", "fragment");
+        volumeFrag = VolumeFragment.newInstance();
         fm = getSupportFragmentManager();
         ft = fm.beginTransaction();
         ft.add(R.id.content_frame, volumeFrag);
--- app/src/main/java/net/ngorham/liquidpackagingcalculator/VolumeFragment.java
+++ app/src/main/java/net/ngorham/liquidpackagingcalculator/VolumeFragment.java
@@ -4,31 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * Liquid Packaging Calculator
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link VolumeFragment.OnVolumeFragmentInteractionListener} interface
- * to handle interaction events.
+ * VolumeFragment.java
+ * Edit
+ * Purpose: A simple {@link Fragment} subclass.
  * Use the {@link VolumeFragment#newInstance} factory method to
  * create an instance of this fragment.
+ *
+ * @author Neil Gorham
+ * @version 1.0 02/17/2019
  */
 public class VolumeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //Private constants
+    private final String TAG = "VolumeFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnVolumeFragmentInteractionListener mListener;
+    //Private variables
+    private RadioGroup fromRadioGroup;
+    private RadioGroup toRadioGroup;
 
     public VolumeFragment() {
         // Required empty public constructor
@@ -38,16 +41,11 @@ public class VolumeFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment VolumeFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static VolumeFragment newInstance(String param1, String param2) {
+    public static VolumeFragment newInstance() {
         VolumeFragment fragment = new VolumeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,55 +53,63 @@ public class VolumeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "INSIDE onCreate: called");
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "INSIDE onCreateView: called");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_volume, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onVolumeFragmentInteraction(uri);
+        View layout = inflater.inflate(R.layout.fragment_volume, container, false);
+        //Set up From RadioGroup
+        fromRadioGroup = (RadioGroup) layout.findViewById(R.id.from_radio_group);
+        toRadioGroup = (RadioGroup) layout.findViewById(R.id.to_radio_group);
+        String[] units = getResources().getStringArray(R.array.volume_units);
+        //create radio buttons
+        for(int i = 0; i < units.length; i++){
+            RadioButton fromRadioButton = new RadioButton(getActivity());
+            RadioButton toRadioButton = new RadioButton(getActivity());
+            fromRadioButton.setText(units[i]);
+            toRadioButton.setText(units[i]);
+            fromRadioButton.setId(i);
+            toRadioButton.setId(i);
+            fromRadioGroup.addView(fromRadioButton);
+            toRadioGroup.addView(toRadioButton);
         }
+        //set listener to radio button groups
+        fromRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int checkedRadioButtonId = fromRadioGroup.getCheckedRadioButtonId();
+                RadioButton radioBtn = (RadioButton) getActivity().findViewById(checkedRadioButtonId);
+                TextView fromSelectedText = (TextView) getActivity().findViewById(R.id.from_selected_text);
+                fromSelectedText.setText(radioBtn.getText());
+                Toast.makeText(getActivity(), "id: " + checkedRadioButtonId + " " + radioBtn.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        toRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int checkedRadioButtonId = toRadioGroup.getCheckedRadioButtonId();
+                RadioButton radioBtn = (RadioButton) getActivity().findViewById(checkedRadioButtonId);
+                TextView toSelectedText = (TextView) getActivity().findViewById(R.id.to_selected_text);
+                toSelectedText.setText(radioBtn.getText());
+                Toast.makeText(getActivity(), "id: " + checkedRadioButtonId + " " + radioBtn.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return layout;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnVolumeFragmentInteractionListener) {
-            mListener = (OnVolumeFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnVolumeFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onVolumeFragmentInteraction(Uri uri);
     }
 }
